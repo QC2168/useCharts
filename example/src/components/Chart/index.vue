d79579cda0edaf2924c33810fd0871e7b1f6e7ff
山田 <huangxinhong2125@gmail.com>
2022-09-09 18:41:07 +0800

    update example

--- example/src/components/Chart/index.vue
+++ example/src/components/Chart/index.vue
@@ -3,79 +3,81 @@
 </template>
 <script setup lang="ts">
 import { onMounted, Ref, ref, computed, nextTick } from "vue";
-import type { EChartsOption } from 'echarts'
-import useChart, { RenderType, ThemeType } from '@qc2168/use-charts'
-import axios from 'axios'
+import type { EChartsOption } from "echarts";
+import useChart from "../../../../dist";
+import axios from "axios";
 
-let w = ref<number>(300)
-let h = ref<number>(300)
+let w = ref<number>(300);
+let h = ref<number>(300);
 
 setInterval(() => {
-    w.value += 100
-    h.value += 100
+    w.value += 100;
+    h.value += 100;
     if (w.value >= 1000) {
-        w.value = 300
-        h.value = 300
+        w.value = 300;
+        h.value = 300;
     }
-}, 1000)
+}, 1000);
 
 // 图表数据
-const barData = ref<number[]>([])
+const barData = ref<number[]>([]);
 
 const option = computed<EChartsOption>(() => ({
     xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
     },
     yAxis: {
-        type: 'value'
+        type: "value",
     },
     series: [
         {
             data: barData.value,
-            type: 'line',
-            smooth: true
-        }
-    ]
+            type: "line",
+            smooth: true,
+        },
+    ],
+}));
 
-}))
+const chartEl = ref<HTMLDivElement | null>(null);
 
-const chartEl = ref<HTMLDivElement | null>(null)
-
-const {
-    setOption,
-    showLoading,
-} = useChart(chartEl as Ref<HTMLDivElement>, true, true, RenderType.SVGRenderer, ThemeType.Light)
+const { setOption, showLoading } = useChart(chartEl as Ref<HTMLDivElement>, {
+    autoChartSize: true,
+    animation: {
+        enable: true,
+        styles: {
+            transition: "all 2s",
+        },
+    },
+});
 
 const getData = async () => {
     try {
-        const res = await axios.get('/api/bar')
+        const res = await axios.get("/api/bar");
         barData.value = res.data.data;
         setOption(option.value);
     } catch {
         setOption({
             title: {
                 show: true,
-                text: '获取数据失败',
-                left: 'center',
-                top: 'center',
+                text: "获取数据失败",
+                left: "center",
+                top: "center",
                 textStyle: {
-                    fontSize: 20
-                }
-            }
+                    fontSize: 20,
+                },
+            },
         });
     }
-}
-
-
+};
 
 onMounted(() => {
     nextTick(() => {
-        showLoading()
-        getData()
-    })
-})
-
+        showLoading();
+        getData();
+    });
+});
 </script>
 <style lang="less" scoped>
+
 </style>
